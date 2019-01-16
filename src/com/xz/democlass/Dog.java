15b8f8354c40f49435acc335df741370456836d5
zs1621 <zs1213yh@gmail.com>
2019-01-17 07:41:21 +0800

    类的多态

--- src/com/xz/democlass/Dog.java
+++ src/com/xz/democlass/Dog.java
@@ -8,8 +8,43 @@ package com.xz.democlass;
  * @date 2019/1/16
  */
 public class Dog extends Animal{
+    public int age = 20;
+
+    @Override
+    public void eat() {
+        System.out.println("Dog吃骨头！");
+    }
     public Dog() {
+        super();
         System.out.println("Dog类执行了");
     }
 
+    /**
+     * 了解super使用
+     */
+    public void method() {
+        System.out.println(super.age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {return true;}
+        if (!(o instanceof Dog)) {return false;}
+
+        Dog dog = (Dog) o;
+
+        return age == dog.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "age=" + age +
+                '}';
+    }
 }
--- src/com/xz/democlass/Init.java
+++ src/com/xz/democlass/Init.java
@@ -7,10 +7,28 @@ package com.xz.democlass;
  * @date 2019/1/16
  */
 public class Init {
+
     public static void main(String[] strings) {
         Dog dog = new Dog();
         System.out.println("狗的年龄:" + dog.age);
         dog.name = "小黄";
         System.out.println("狗的昵称:" + dog.name);
+        dog.method();
+
+        System.out.println(dog);
+
+        // 重写equals 判断属性值age
+        Dog dog2 = new Dog();
+        dog.age = 12;
+        dog2.age = 12;
+        System.out.println(dog.equals(dog2));
+
+
+        // 关于多态
+        Animal a1 = new Animal();
+        Animal a2 = new Dog();
+
+        a1.eat();
+        a2.eat();
     }
 }
