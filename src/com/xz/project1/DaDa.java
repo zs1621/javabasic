f2d0cc786d4939708c98aa186a8c66ab90730ca6
zs1621 <zs1213yh@gmail.com>
2019-01-24 08:30:38 +0800

    类练习题 答答租车系统完成

--- src/com/xz/project1/DaDa.java
+++ src/com/xz/project1/DaDa.java
@@ -1,6 +1,7 @@
 package com.xz.project1;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * 答答租车系统
@@ -17,8 +18,8 @@ public class DaDa {
      * @param days 所需天数
      * @return 总价格
      */
-    public double calPriceTotal(Car[] cars, int days) {
-        Double priceTotal = 0.0;
+    private static double calPriceTotal(Car[] cars, int days) {
+        double priceTotal = 0.0;
         for (Car car : cars) {
             priceTotal += car.price * days;
         }
@@ -31,12 +32,29 @@ public class DaDa {
      * @param cars 所选车型列表
      * @return 总载客量
      */
-    public int calPassengerCapacity(Car[] cars) {
+    private static int calPassengerCapacity(ArrayList cars) {
         int passengerCount = 0;
-        for (Car car : cars) {
+        for (int i=0; i < cars.size(); i++) {
+            Car car = (Car)cars.get(i);
             passengerCount += car.passengerCapacity;
         }
         return passengerCount;
+    }
+
+    /**
+     * 计算总载重量
+     *
+     * @param cars 车数组
+     * @return 总载重量
+     */
+    private static double calCargoCapacity(ArrayList cars) {
+        double  cargoCapacity = 0.0;
+        for (int i=0; i < cars.size(); i++) {
+            Car car = (Car)cars.get(i);
+            cargoCapacity += car.cargoCapacity;
+        }
+        return cargoCapacity;
+
     }
 
     public static void main(String[] strings) {
@@ -70,6 +88,45 @@ public class DaDa {
                 // 异常判断
                 carsRentIndex[i] = carIndex;
             }
+
+            System.out.println("请输入租车的天数:");
+            int day;
+            day = input.nextInt();
+
+            // 找出载客车和载货车
+            ArrayList passengerCarRent = new ArrayList();
+            ArrayList goodsCarRent = new ArrayList();
+            Car[] carsRent = new Car[response];
+            int carRentIndex = 0;
+            for (int i: carsRentIndex) {
+                if (cars[i-1].isCargo()) {
+                    goodsCarRent.add(cars[i-1]);
+                }
+                if (cars[i-1].isPassenger()) {
+                    passengerCarRent.add(cars[i-1]);
+                }
+                carsRent[carRentIndex] = cars[i-1];
+                carRentIndex += 1;
+            }
+
+            // 统计订单信息
+
+            System.out.println("您的订单信息:");
+            System.out.println("***可载人的车有:");
+            for (int i=0; i < passengerCarRent.size(); i++) {
+                Car car = (Car)passengerCarRent.get(i);
+                System.out.printf("%s  ", car.name);
+            }
+            System.out.printf("共载人 %d人%n", calPassengerCapacity(passengerCarRent));
+
+            System.out.println("***载货的车有:");
+            for (int i=0; i< goodsCarRent.size(); i++) {
+                Car car = (Car)goodsCarRent.get(i);
+                System.out.printf("%s  ", car.name);
+            }
+            System.out.printf("共载货 %.2f吨%n",  calCargoCapacity(goodsCarRent));
+
+            System.out.printf("***租车的总价格为:%n %.2f", calPriceTotal(carsRent, day));
         }
 
 
